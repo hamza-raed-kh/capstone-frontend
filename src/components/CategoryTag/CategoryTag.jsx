27ec0ab424b643afcfe5bb698efcf09ef1b40ff7
amr-ai2004 <amraimahadeen@gmail.com>
2026-04-14 00:42:12 +0300

    Sprint2: Event Card Variant (& Completion)

--- src/components/CategoryTag/CategoryTag.jsx
+++ src/components/CategoryTag/CategoryTag.jsx
@@ -1,9 +1,16 @@
-import './CategoryTag.css'
+import style from './CategoryTag.module.css'
 
+/**
+ * A categorytag component with a single visual style.
+ *
+ * @param {object} props - The properties for the categorytag.
+ * @param {string} props.text - The category name to be displayed inside the tag.
+ * @returns {JSX.Element} The rendered categorytag element.
+ */
 function CategoryTag({text}){
     return <>
-        <div className="category">
-            <span className="category-text">
+        <div className={`${style.category}`}>
+            <span className={`${style.categoryText}`}>
                 {text}
             </span>
         </div>
